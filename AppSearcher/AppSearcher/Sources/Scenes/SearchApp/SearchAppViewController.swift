f956e051cdf03162a279263d8c0d9ce21a55c47e
iDoyoung <ido.young@icloud.com>
2022-08-04 15:29:08 +0900

    Initial commit

---
+++ AppSearcher/AppSearcher/Sources/Scenes/SearchApp/SearchAppViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSearcher
+//
+//  Created by Doyoung on 2022/08/04.
+//
+
+import UIKit
+
+class SearchAppViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

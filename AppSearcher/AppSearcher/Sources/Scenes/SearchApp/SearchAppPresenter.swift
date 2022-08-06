d73b3b684d14bd5f8f3f6c13d0ee14caaec00298
iDoyoung <ido.young@icloud.com>
2022-08-06 18:20:05 +0900

    Implement Search app interactor

---
+++ AppSearcher/AppSearcher/Sources/Scenes/SearchApp/SearchAppPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAppPresenter.swift
+//  AppSearcher
+//
+//  Created by Doyoung on 2022/08/06.
+//
+
+import Foundation
+
+protocol SearchAppPresentionLogic {
+    func present()
+}
+
+final class SearchAppPresenter: SearchAppPresentionLogic {
+    func present() {
+        
+    }
+}

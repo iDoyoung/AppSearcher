06177b9283895bc56a81a303bb1ec3c491a79dd9
iDoyoung <ido.young@icloud.com>
2022-08-07 15:45:15 +0900

    Add Search controller in Search app view controller

--- AppSearcher/AppSearcher/Sources/Scenes/SearchApp/SearchAppViewController.swift
+++ AppSearcher/AppSearcher/Sources/Scenes/SearchApp/SearchAppViewController.swift
@@ -12,12 +12,60 @@ protocol SearchAppDisplayLogic: AnyObject {
 }
 
 class SearchAppViewController: UIViewController {
-
+    var interactor: (SearchAppBussinessLogic&SearchAppDataStore)?
+    //MARK: - UI components property
+    var searchController = UISearchController()
+    
+    //MARK: - Life cyle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setupVC()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupVC()
+    }
+    override func loadView() {
+        super.loadView()
+        view.backgroundColor = .systemBackground
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.searchController = searchController
+        searchController.searchBar.delegate = self
+        setupUIComponents()
     }
-
-
+    //TODO: - DI 구현시 setupVC() 제거
+    func setupVC() {
+        let networkConfiguration = NetworkConfiguration(baseURL: URL(string: "http://itunes.apple.com/")!)
+        let networkService = NetworkService(configuration: networkConfiguration)
+        let worker = SearchedAppWorker(networkService: networkService)
+        let presenter = SearchAppPresenter(self)
+        let interactor = SearchAppInteractor(searchedAppWorker: worker, presenter: presenter)
+        self.interactor = interactor
+    }
+    private func setupUIComponents() {
+        setupSearchController()
+        setupLayoutConstraint()
+    }
+    private func setupLayoutConstraint() {
+        NSLayoutConstraint.activate([
+        ])
+    }
+    private func setupSearchController() {
+        searchController.searchBar.placeholder = "ex) 872469884 = 아이디어스"
+    }
 }
 
+extension SearchAppViewController: SearchAppDisplayLogic {
+    func displaySuccessSearching() {
+        print("Success Searcing")
+    }
+}
+
+extension SearchAppViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        interactor?.searchApp(with: searchBar.text!)
+    }
+}
